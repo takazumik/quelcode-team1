47cf1acba99f15f779e37ffbe61ee67987781a32
shootaa <60875264+shootaa@users.noreply.github.com>
2020-09-07 20:41:15 +0900

    ・マイグレーション ファイルの追加。 ・ER図の追加。

---
+++ html/mycakeapp/config/Migrations/20200907110012_CreateUsers.php
@@ -0,0 +1,46 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('is_deleted', 'boolean', [
+            'default' => null,
+            'limit' => 1,
+            'null' => false,
+        ]);
+        $table->addColumn('is_registrated', 'boolean', [
+            'default' => null,
+            'limit' => 1,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
