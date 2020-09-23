b1970ff058e53daaa7f3d128dc06bc435bb50123
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-09-23 09:27:02 +0900

    add.ctpでのエラーメッセージのレイアウト崩れを改善（コントローラー改変）

--- html/mycakeapp/src/Controller/UsersController.php
+++ html/mycakeapp/src/Controller/UsersController.php
@@ -30,10 +30,7 @@ class UsersController extends AppController
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->getData());
             if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
                 $this->setAction('mail', $user);
-            } else {
-                $this->Flash->error(__('The user could not be saved. Please, try again.'));
             }
         }
     }
